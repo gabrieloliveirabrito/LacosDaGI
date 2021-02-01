d69d742a6b25feb0fdc52890f9446f709bccc860
gabrieloliveirabrito <gabriel.oliveira.brito@outlook.com>
2021-02-01 17:51:37 -0300

    Componentes atalizados, splash e dialogos

---
+++ LacosDaGI/LacosDaGI/Views/Main/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LacosDaGI.Views.Main
+{
+    using Dependencies;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Application.Current.RequestedThemeChanged += Current_RequestedThemeChanged;
+            Current_RequestedThemeChanged(this, new AppThemeChangedEventArgs(Application.Current.RequestedTheme));
+        }
+
+        private void Current_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            var darkColor = (Color)Application.Current.Resources["SecondaryDarkMainColor"];
+            var lightColor = (Color)Application.Current.Resources["SecondaryLightMainColor"];
+
+            StatusBar.SetStatusBarColor(e.RequestedTheme == OSAppTheme.Dark ? darkColor : lightColor);
+        }
+    }
+}
